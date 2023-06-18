74bb5c4b914f397195da3225a42dc6f6c620573a
LaideeBudd <107522099+LaideeBudd@users.noreply.github.com>
2023-06-17 19:49:51 -0500

    treasure interacitvity

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ const menuToggle = () => {
     menuItems.style.display = "none";
   }
 };
+
+const key = document.getElementById("key");
+const treasureHeading = document.getElementById("treasure-heading");
+const treasureSubheading = document.getElementById("treasure-subheading");
+const chest = document.getElementById("chest");
+
+const openChest = () => {
+  userAnswer = key.value;
+  if (userAnswer === "password") {
+    console.log(key);
+    treasureHeading.innerHTML = "Congratulations!";
+    treasureSubheading.innerHTML = "You have opened the treasure chest!";
+    chest.src = "./images/treasurechest.png";
+  }
+};
