2628ddea632ff4ae2c7c0ad46a4c795ec8d05a65
LaideeBudd <107522099+LaideeBudd@users.noreply.github.com>
2023-06-25 15:15:30 -0500

    added real password

--- script.js
+++ script.js
@@ -14,7 +14,31 @@ const chest = document.getElementById("chest");
 
 const openChest = () => {
   userAnswer = key.value;
-  if (userAnswer === "password") {
+  if (userAnswer === "pirateShipPirateBuriedTreasure") {
+    console.log(key);
+    treasureHeading.innerHTML = "Congratulations!";
+    treasureSubheading.innerHTML = "You have opened the treasure chest!";
+    chest.src = "./images/treasurechest.png";
+    chest.style.marginTop = "8rem";
+  }
+
+  if (userAnswer === "pirate ship pirate buried treasure") {
+    console.log(key);
+    treasureHeading.innerHTML = "Congratulations!";
+    treasureSubheading.innerHTML = "You have opened the treasure chest!";
+    chest.src = "./images/treasurechest.png";
+    chest.style.marginTop = "8rem";
+  }
+
+  if (userAnswer === "pirateshippirateburiedtreasure") {
+    console.log(key);
+    treasureHeading.innerHTML = "Congratulations!";
+    treasureSubheading.innerHTML = "You have opened the treasure chest!";
+    chest.src = "./images/treasurechest.png";
+    chest.style.marginTop = "8rem";
+  }
+
+  if (userAnswer === "PirateShipPirateBuriedTreasure") {
     console.log(key);
     treasureHeading.innerHTML = "Congratulations!";
     treasureSubheading.innerHTML = "You have opened the treasure chest!";
